fbe5a49c132084c0e7c373a9fd1c1b405df395a5
Javier Olmo Injerto <44966721+javierolmo@users.noreply.github.com>
2022-05-15 21:49:32 +0200

    Arreglo ej 15.1

--- src/main/java/JLMLt15/JLMLt15e01.java
+++ src/main/java/JLMLt15/JLMLt15e01.java
@@ -1,19 +1,23 @@
 /* */
 package JLMLt15;
 
+import java.util.LinkedList;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class JLMLt15e01 {
-static JLMLt15.LinkedListParking p;
-    static Scanner teclado;
+
+    private static Scanner teclado = new Scanner(System.in);
+    private static LinkedList<String> parking = new LinkedList<>();
+    private static final int MAX_PLAZAS = 10;
+
     public static void main(String[] args) {
-        p=new JLMLt15.LinkedListParking();
-        boolean salir =false; char opcion;
-        teclado = new Scanner(System.in);
+
+        boolean salir =false;
         do{
             System.out.println("\n\n---GESTIÓN PARKING---");
             System.out.println("a)Mostrar parking\nb)Aparcar\nc)Desaparcar\nd)Plazas Libres\ne)Salir");
-            opcion =teclado.next().charAt(0);
+            char opcion =teclado.next().charAt(0);
             switch (opcion) {
                 case 'a': mostrarParking(); break;
                 case 'b': aparcar(); break;
@@ -22,32 +26,45 @@ static JLMLt15.LinkedListParking p;
                 case 'e': salir=true; break;
                 default: System.out.println("opción inválida");
             }                
-            } while (!salir);
+        } while (!salir);
     }
-            private static void mostrarParking(){
-                String[]arr;
-                arr=p.mostrarParking();
-                System.out.println("Estado del Parking");
-                for (int i=0;i<arr.length;i++)
-                    System.out.println((i+1)+"::"+arr[i]);
-                
-            }
-            
-            private static void aparcar(){
-                System.out.println("Introduce matrícula: ");teclado.nextLine();
-                String mat=teclado.nextLine();
-                int res = p.Aparcar(mat);
-                if(res==-1)System.out.println("Parking lleno");
-                else System.out.println("Coche aparcado en plaza "+ res);
-            }
-            private static void desaparcar(){
-                String mat=p.Desaparcar();
-                if(mat==null)System.out.println("Parking vacío");
-                else System.out.println("coche: "+mat+ " desaparcado");
-                
-            }
-            private static void plazasLibres(){ 
-                int q=p.cantidadPlazas();
-                System.out.println(q+" plazas libres");
-            }
+
+    private static void mostrarParking(){
+        LinkedList<String> resultado = new LinkedList<>(parking); // Copio lista y trabajo sobre la copia
+        while (resultado.size() < 10) {
+            resultado.addFirst("[VACIO]");
         }
+        System.out.println("El estado actual del parking es:");
+        System.out.println(String.join(" | ", resultado));
+        System.out.println();
+    }
+
+    private static void aparcar(){
+        System.out.print("Introduce matrícula: ");
+        String matricula=teclado.next();
+        if(parking.size() >= 10) {
+            System.out.println("Error. Parking lleno!");
+        } else {
+            parking.addFirst(matricula);
+            System.out.println("Coche aparcado con éxito. Quedan " + (MAX_PLAZAS - parking.size()) + " plazas");
+        }
+        System.out.println();
+    }
+
+    private static void desaparcar(){
+        try {
+            String matricula = parking.removeFirst();
+            System.out.println("Se ha desaparcado el coche " + matricula + ".  Quedan " + (MAX_PLAZAS - parking.size()) + " plazas");
+        } catch (NoSuchElementException nsee) {
+            System.out.println("Error. El parking está vacío.");
+        } finally {
+            System.out.println();
+        }
+
+    }
+
+    private static void plazasLibres(){
+        System.out.println("Quedan " + (MAX_PLAZAS - parking.size()) + " plazas libres.");
+    }
+
+}
